7c5026463561e92be84d21f375509dcef1f6ff12
Freddie <freddiejmo@gmail.com>
2019-05-27 11:46:18 -0400

    updating posts list frontend after delete post

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -26,7 +26,7 @@ export class PostListComponent implements OnInit, OnDestroy{
 
   onDelete(postId: string) {
     this.postsService.deletePost(postId);
-  };
+  }
 
   ngOnDestroy() {
     this.postsSub.unsubscribe();
