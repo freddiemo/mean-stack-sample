baeaf1e86a8e7ebdac03bd87c9c2983aa54e34c7
Freddie <freddiejmo@gmail.com>
2019-05-26 22:48:13 -0400

    placeholder in post-create and edit, delete button in post-list

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -19,6 +19,7 @@ export class PostCreateComponent {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.title);
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
