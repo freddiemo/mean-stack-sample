23720650e3c1546ed819da4d88c710dbaa37666b
Freddie <freddiejmo@gmail.com>
2019-05-27 13:58:22 -0400

    adde update post

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+    'GET, POST, PUT, PATCH, DELETE, OPTIONS'
   );
   next();
 });
@@ -43,6 +43,17 @@ app.post('/api/posts', (req, res, next) => {
   });
 });
 
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id}, post).then(result => {
+    res.status(200).json({message: 'Update succesfull!'});
+  });
+});
+
 app.get(('/api/posts'), (req, res, next) => {
   Post.find()
   .then(documents => {
@@ -50,6 +61,16 @@ app.get(('/api/posts'), (req, res, next) => {
       message: 'Posts fetched succesfully!',
       posts: documents
     });
+  });
+});
+
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({message: 'Post not found!'})
+    }
   });
 });
 
